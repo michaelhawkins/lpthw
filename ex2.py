94640afa8101a1054c66593b6b8c24e356deac45
Michael Hawkins <michael.hawkins@lendkey.com>
2017-03-27 09:53:36 -0400

    initial commit

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# This is my first comment
+# I'm glad to be back at Python
+print "Here's the first line in ex2.py of Learn Python the Hard Way"
+
+# Some more comments for Python
+print "Here's the second line in ex2.py of Learn Python the Hard Way"
